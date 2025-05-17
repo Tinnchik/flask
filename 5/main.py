7300b077fab6bec155d0d8f1754235474390370a
tinnec <limer2838@gmail.com>
2025-05-18 00:20:36 +0300

    da

---
+++ 5/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+PROFESSIONS = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
+               'инженер по терраформированию', 'климатолог', 'специалист по радиационной защите',
+               'астрогеолог', 'гляциолог', 'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода',
+               'киберинженер', 'штурман', 'пилот дронов']
+
+
+@app.route('/login')
+def login():
+    params = {'emblem_url': url_for('static', filename='img/emblem.png')}
+    return render_template('emergency_access.html', **params)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8800)
