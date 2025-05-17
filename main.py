938c4ca8abe1115d41a6f965cd43c464eed02b26
tinnec <limer2838@gmail.com>
2025-05-17 22:40:23 +0300

    da

--- main.py
+++ main.py
@@ -151,6 +151,7 @@ def choice(name, level, rating):
   </body></html>
 '''
 
+
 @app.route('/load_photo', methods=['POST', 'GET'])
 def load_photo():
     if request.method == 'GET':
@@ -183,6 +184,49 @@ def load_photo():
             f.write(f1.read())
         return '''<img src="{url_for('static', filename='img/img.jpg')}" 
                    alt="здесь должна была быть картинка, но не нашлась">'''
+
+
+@app.route('/carousel', methods=['POST', 'GET'])
+def carousel():
+    return '''<html lang="en"><head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <body>
+  <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+    </head>
+    <div class="p-5">
+      <h1 class="text-center">Пейзажи Марса</h1>
+
+      <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active" data-bs-interval="500">
+            <img src="https://i.pinimg.com/736x/b7/e6/c7/b7e6c71b5ce0c2e509bad526915961fd.jpg" class="d-block w-100">
+          </div>
+          <div class="carousel-item" data-bs-interval="500">
+            <img src="https://cdna.artstation.com/p/assets/images/images/028/598/264/large/knife-entertainment-frame-0309.jpg?1594922978" class="d-block w-100">
+          </div>
+          <div class="carousel-item" data-bs-interval="500">
+            <img src="https://i.ytimg.com/vi/w3vRyGLnqfs/maxresdefault.jpg" class="d-block w-100">
+          </div>
+          <div class="carousel-item" data-bs-interval="500">
+            <img src="https://thumbs.dreamstime.com/b/desert-mars-landscapes-sand-rocks-generative-ai-268284761.jpg" class="d-block w-100">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  
+  </body></html>'''
 
 
 if __name__ == '__main__':
